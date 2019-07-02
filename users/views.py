38e7e6aee8684a03a9419ed94c7f70f8733b1bc9
dominik <donpiet@wp.pl>
2019-07-02 15:21:05 +0200

    4.0 formularz update

--- users/views.py
+++ users/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Termin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .forms import CreatTermin
 from django.shortcuts import get_object_or_404
 
@@ -27,19 +25,32 @@ def profile(request):
     return render(request, 'users/profile.html')
 
 def termin_list_view(request):
-    return render(request, 'users/termin.html', {
+    return render(request, 'users/terminy.html', {
         'terminy': Termin.objects.filter(author=request.user)
     })
 
 def termin_detail_view(request):
-    return render(request, 'users/termin.html', {
+    return render(request, 'users/terminy.html', {
         'terminy': Termin.objects.filter(author=request.user)
     })
 
-def termin_delete_view(request, pk):
-    return render(request, 'users/termin.html', {
-        'terminy': Termin.objects.filter(author=request.user)
-    })
+def termin_delete(request, pk):
+    termin = get_object_or_404(Termin, pk=pk)
+    if request.method == 'POST':
+        termin.delete()
+        return redirect(request, 'users/terminy.html')
+    context = {
+        't': termin
+    }
+    return redirect(request, 'users/terminy.html', context)
+
+# def termin_delete(request, pk):
+#     try:
+#         termin = Termin.objects.get(pk=pk).delete()
+#     except Termin.DoesNotExist:
+#         print("no such object")
+#         return redirect('users/terminy.html')
+#     return redirect(request, 'users/terminy.html')
 
 def termin_create_view(request):
     form_class = CreatTermin
@@ -49,13 +60,14 @@ def termin_create_view(request):
         if form.is_valid():
             termin = Termin(
                 author = request.user,
+                date = form.cleaned_data['date'],
                 mobile_number = form.cleaned_data['mobile_number'],
                 status = form.cleaned_data['status'],
                 price = form.cleaned_data['price']
             )
             termin.save()
-            return redirect('profile')
-    return render(request, 'users/termin_form.html', {
+            return redirect('/terminy')
+    return render(request, 'users/terminy_form.html', {
         'form': form
     })
 
@@ -65,7 +77,7 @@ def termin_update_view(request, pk):
         termin = Termin.objects.get(pk=pk, author=request.user)
     except Termin.DoesNotExist:
         print("no such object")
-        return redirect('termin')
+        return redirect('/terminy')
     return render(request, 'users/update_termin.html', {
         't': termin
     })
@@ -80,38 +92,9 @@ def termin_edit(request, pk):
                 status=form.cleaned_data['status'],
                 price=form.cleaned_data['price'],
                 termin.save(),
-                return redirect('terminy')
+                return redirect('/terminy')
     else:
         form = CreatTermin(instance=termin)
     return render(request, 'users/update_termin.html', {'form': form})
 
-# class TerminListUpdate(LoginRequiredMixin, CreateView):
-#     model = Termin
-#     fields = ['status', 'mobile_number', 'price']
-#
-#     def form_valid(self, form):
-#         form.instance.author = self.request.user
-#         return super().form_valid(form)
 
-def show_termin(request):
-    content = {
-        'termin': Termin.objects.all()
-    }
-    return render(request, 'users/termin.html', content)
-#
-# def termin_update_view(request, pk):
-#     form_class = CreatTermin
-#     form = form_class(request.POST or None)
-#     if request.method == 'POST':
-#         form = CreatTermin(request.POST)
-#         if form.is_valid():
-#             termin = Termin(
-#                 author=request.user,
-#                 mobile_number=form.get['mobile_number'],
-#                 status=form.get['status'],
-#                 price=form.GET['price']
-#             )
-#             return redirect('profile')
-#     return render(request, 'users/update_termin.html', {
-#         'form': form
-#     })
